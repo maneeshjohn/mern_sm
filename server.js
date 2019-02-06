25065e7e66c9def931e72cc2cfe9f68030d9bc50
Maneesh John <maneeshjohno@gmai.com>
2019-02-06 13:07:01 +0530

    db connection established

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const db = require('./config/config').dbURI;
+
+mongoose.connect(db)
+  .then(() => console.log('connected successfully to db...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'))
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server listening at ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
