19a262bdcef38cc7447692b15a9dd71cd5397065
Maneesh John <maneeshjohno@gmai.com>
2019-02-07 09:45:02 +0530

    user login and registeration implemented

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -3,36 +3,58 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
-const userController = (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if(user) {
-        return res.status(400).json({ email: 'Email already exists' });
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: '200',  //  sixe
-          r: 'pg',  //  rating
-          d: 'mm'  //  default
-        });
+const usersController = {
+  registerNewUser: function(req, res){
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if(user) {
+          return res.status(400).json({ email: 'Email already exists' });
+        } else {
+          const avatar = gravatar.url(req.body.email, {
+            s: '200',  //  size
+            r: 'pg',  //  rating
+            d: 'mm'  //  default
+          });
 
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        })
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => res.send(err));
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            avatar,
+            password: req.body.password
           })
-        })
-      }
-    })
+
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => res.json(user))
+                .catch(err => res.send(err));
+            })
+          })
+        }
+      })
+  },
+
+  loginUser: function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+      .then(user => {
+        if(!user) {
+          return res.status(404).json({ email: "User doesn't exist" });
+        }
+        bcrypt.compare(password, user.password)
+          .then(success => {
+            if(success) {
+              res.json({ msg: 'Success' })
+            } else {
+              return res.status(400).json({ msg: 'Invalid password' });
+            }
+          })
+      })
+  }
 }
 
-module.exports = userController;
+module.exports = usersController;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const usersController = require('../api/controllers/usersController')
-
+const usersController = require('../api/controllers/usersController');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('User works'));
 
-router.post('/register', (req, res) => usersController(req, res));
+router.post('/register', (req, res) => usersController.registerNewUser(req, res));
+
+router.post('/login', (req, res) => usersController.loginUser(req,res));
 
 module.exports = router;
