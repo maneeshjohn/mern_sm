38042dd0bcbe40aef4a2ba25b0517c218ae7591d
maneeshjohn <maneeshjohno@gmail.com>
2019-02-19 10:21:29 +0530

    redux and react router setup completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import store from './redux/store/store'
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React</h1>
-      </div>
+      <Provider store={ store }>
+        <BrowserRouter>
+          <div className="App">
+            <h1>React</h1>
+          </div>
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
