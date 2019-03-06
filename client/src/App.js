610f25813f71c8d360a83613a9c779970d0e2c42
maneeshjohn <maneeshjohno@gmail.com>
2019-03-06 14:18:32 +0530

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { Provider } from 'react-redux'
 import store from './redux/store/store'
 import { BrowserRouter } from 'react-router-dom'
 
+import Landing from './components/landing/Landing'
+
 class App extends Component {
   render() {
     return (
       <Provider store={ store }>
         <BrowserRouter>
-          <div className="App">
-            <h1>React</h1>
+          <div className="App">            
+            <Landing />
           </div>
         </BrowserRouter>
       </Provider>
---
+++ client/src/components/landing/Landing.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Mainbar from './Mainbar'
+
+export default () =>
+  <div className="landing">
+    <Mainbar />
+  </div>
